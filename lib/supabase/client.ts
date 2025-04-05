ac87ffe5ce978d453d17ef8a35763b23dced86cb
Ezra <128621626+hnxzl@users.noreply.github.com>
2025-04-05 20:25:40 +0700

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,9 @@
 import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 
 export const createClient = () => {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+  const supabaseUrl = "https://fmguyccbzagntdarepby.supabase.co";
+  const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtZ3V5Y2NiemFnbnRkYXJlcGJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2MTYyNjcsImV4cCI6MjA1OTE5MjI2N30.eQhUFFLuiBFEzGSMbj33IONyW0JYTdnSXdzvukezVAc";
+
   return createSupabaseClient(supabaseUrl, supabaseKey);
 };
